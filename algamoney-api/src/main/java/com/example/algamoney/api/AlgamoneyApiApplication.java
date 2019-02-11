904065ec3d036628557f5c1da2b4dfbbfc9f29c6
Rodrigo <Rodrigo@Rodrigo-PC>
2019-02-11 15:55:14 -0200

    Primeira carga

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -0,0 +1,25 @@
+package com.example.algamoney.api;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@SpringBootApplication
+public class AlgamoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+
+	@Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+}
+
---
+++ algamoney-api/src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -0,0 +1,68 @@
+package com.example.algamoney.api.exceptionhandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getCause().toString();
+		
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	/*
+	 * @Override protected ResponseEntity<Object>
+	 * handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders
+	 * headers, HttpStatus status, WebRequest request) {
+	 * 
+	 * List<Erro> erros = new ArrayList<>();
+	 * 
+	 * return handleExceptionInternal(ex, erros, headers, status,
+	 * HttpStatus.BAD_REQUEST); }
+	 */
+	
+	public static class Erro {
+		
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+	
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+		
+		
+		
+		
+		
+	}
+
+}
